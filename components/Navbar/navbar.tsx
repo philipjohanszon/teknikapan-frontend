d68a2785420cfe0ab45222e2bee2acc0bb7a5abd
Philip Johansson <philip.hlm@live.se>
2022-03-29 02:10:35 +0200

    fixed styled components

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -7,9 +7,25 @@ const StyledNavbar = styled.nav`
     justify-content: space-between;
     padding: 0.5rem 1rem;
     position: fixed;
-    z-index: 1;
     top: 0;
     width: 100%;
+    height: 80px;
+`;
+
+const NavbarMargin = styled.div`
+    margin-top: 80px;
+`;
+
+const MobileNavbar = styled.div`
+    background-color: blue;
+    border-bottom: 1px solid #e5e5e5;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 1rem;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 80px;
 `;
 
 interface NavbarProps {
@@ -17,7 +33,18 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC = ({ children }) => {
-    return <><StyledNavbar>Hey </ StyledNavbar>{children}</>;
+    return (
+            <>
+                <StyledNavbar>
+                    Normal nav
+                </ StyledNavbar>
+                <NavbarMargin />
+                {children}
+                <MobileNavbar>
+                    Mobile nav     
+                </ MobileNavbar>
+            </>
+        );
 }
 
 export default Navbar;
